44dbfb8a96549f53d241100e2e815d81b50c88ee
jcy010905 <jcy010905@naver.com>
2024-01-09 21:21:09 +0900

    찬용이가 개발함

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>하아핸ㅇ흥내한애한애하ㅐㄴㅇㅎ</p>
+        <p>찬용쓰</p>
         <a
           className="App-link"
           href="https://reactjs.org"
