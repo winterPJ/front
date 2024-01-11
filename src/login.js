30bb0086bca17f20be8f8a028e3d4c387297b265
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-11 12:22:49 +0900

    login

--- src/login.js
+++ src/login.js
@@ -1,48 +1,62 @@
 import { useState } from "react";
-import "./SignUp.css"
+import "./SignUp.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    function registerHandler() {
-
-        if(email == "" || password == "" ) {
-            alert("모든 정보를 입력해주세요!");
-            return;
-        }
-
-        fetch(`http://back.mongjo.xyz/user/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            }, 
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            })
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-        });
+  function registerHandler(event) {
+    event.preventDefault();
+    if (email === "" || password === "") {
+      alert("모든 정보를 입력해주세요!");
+      return;
     }
 
-    return (
-        <div className="wrapper">
-            <div className="login-wrapper">
-                <h2>로그인</h2>
-                    <form id="login-form" onSubmit={registerHandler}>
-                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                        <input
-                            type="submit"
-                            value="로그인"
-                            />
-                        <p><a href="/">메인 화면으로 돌아가기</a></p>
-                    </form>
-            </div>
-        </div>
-    );
+    fetch(`http://back.mongjo.xyz/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res["success"] === true) {
+          alert(res["data"]);
+          navigate("/");
+        } else {
+          alert(res["data"]);
+        }
+      });
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="login-wrapper">
+        <h2>로그인</h2>
+        <form id="login-form" onSubmit={registerHandler}>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value="로그인" />
+          <p>
+            <a href="/">메인 화면으로 돌아가기</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,103 +1,128 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import NavbarLogin from './navbarlogin';
-import Navbar from './navbar';
-import './mainpage.css';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NavbarLogin from "./navbarlogin";
+import Navbar from "./navbar";
+import "./mainpage.css";
 
 function MainPage() {
-    const [posts, setPosts] = useState([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
-    const [hotPosts, setHotPosts] = useState([]);
-    const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [hotPosts, setHotPosts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        /* const sessionToken = sessionStorage.getItem('sessionToken');
+  useEffect(() => {
+    fetch("http://back.mongjo.xyz/user/check", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.info(res["success"] + "ss");
+        res["success"] ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    /* const sessionToken = sessionStorage.getItem('sessionToken');
         setIsLoggedIn(!!sessionToken); */
 
-        fetch("http://back.mongjo.xyz/post/get")
-            .then(response => response.json())
-            .then(data => {
-               setPosts(data);
-               const sortedPosts = [...data].sort((a, b) => b.comment_cnt - a.comment_cnt);
-                setHotPosts(sortedPosts.slice(0, 3));
-            })
-            .catch(error => console.error("Fetching posts failed:", error));
-    }, []);
+    fetch("http://back.mongjo.xyz/post/get")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        const sortedPosts = [...data].sort(
+          (a, b) => b.comment_cnt - a.comment_cnt
+        );
+        setHotPosts(sortedPosts.slice(0, 3));
+      })
+      .catch((error) => console.error("Fetching posts failed:", error));
+  }, []);
 
-    const goToPostingPage = () => {
-        navigate('/posting');
-    };
+  const goToPostingPage = () => {
+    navigate("/posting");
+  };
 
-    const goToHotPostDetail = (postId) => {
-        navigate(`/detailpost/${postId}`);
-    };
+  const goToHotPostDetail = (postId) => {
+    navigate(`/detailpost/${postId}`);
+  };
 
-    const goToDetailPage = (postId) => {
-        navigate(`/detailpost/${postId}`);
-    };
+  const goToDetailPage = (postId) => {
+    navigate(`/detailpost/${postId}`);
+  };
 
-    const calcTime = (datetime) => {
-        const inputDate = new Date(datetime).getTime();
-        const currentDate = new Date().getTime();
-        const diff = currentDate - inputDate;
+  const calcTime = (datetime) => {
+    const inputDate = new Date(datetime).getTime();
+    const currentDate = new Date().getTime();
+    const diff = currentDate - inputDate;
 
-        const minutes = Math.floor(diff / 60000);
-        const hours = Math.floor(diff / 3600000);
-        const days = Math.floor(diff / 86400000);
+    const minutes = Math.floor(diff / 60000);
+    const hours = Math.floor(diff / 3600000);
+    const days = Math.floor(diff / 86400000);
 
-        if (minutes < 60) {
-            return `${minutes}분 전`;
-        } else if (hours < 24) {
-            return `${hours}시간 전`;
-        } else if (days < 7) {
-            return `${days}일 전`;
-        } else {
-            const date = new Date(datetime);
-            return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
-        }
-    };
+    if (minutes < 60) {
+      return `${minutes}분 전`;
+    } else if (hours < 24) {
+      return `${hours}시간 전`;
+    } else if (days < 7) {
+      return `${days}일 전`;
+    } else {
+      const date = new Date(datetime);
+      return `${date.getFullYear()}-${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+    }
+  };
 
-    return (
-        <div>
-            {isLoggedIn ? <Navbar /> : <NavbarLogin />}
-            { /* <Navbar />
-            <NavbarLogin /> */ }
+  return (
+    <div>
+      {isLoggedIn ? <Navbar /> : <NavbarLogin />}
+      {/* <Navbar />
+            <NavbarLogin /> */}
 
-            <div className="pageLayout">
-                <div className="sidebar">게시판 참여자 목록</div>
-                <div className="mainContent">
-                    <h3>게시글 현황</h3>
-                    {Array.isArray(posts) && posts.map(post => (
-                        <div key={post.id} className="post" onClick={() => goToDetailPage(post.id)}>
-                            <h4>{post.title}</h4>
-                            <p>{post.body}</p>
-                            <div className="commentCount">
-                                댓글 수: {post.comment_cnt}
-                            </div>
-                            <div className="postTime">
-                                {calcTime(post.created_at)}
-                            </div>
-                        </div>
-                    ))}
-                    {isLoggedIn && (
-                    <div className="writeButtonContainer">
-                        <button onClick={goToPostingPage} className="writeButton">글쓰기</button>
-                    </div>
-            )}
-                </div>
-                <div className="sidebar">
-                    <h4>핫게</h4>
-                        <ul>
-                            {hotPosts.map((post, index) => (
-                                <li key={post.id} onClick={() => goToHotPostDetail(post.id)} style={{ cursor: 'pointer' }}>
-                                    {index + 1}등: {post.title}
-                                </li>
-                            ))}
-                        </ul>
-                </div>
+      <div className="pageLayout">
+        <div className="sidebar">게시판 참여자 목록</div>
+        <div className="mainContent">
+          <h3>게시글 현황</h3>
+          {Array.isArray(posts) &&
+            posts.map((post) => (
+              <div
+                key={post.id}
+                className="post"
+                onClick={() => goToDetailPage(post.id)}
+              >
+                <h4>{post.title}</h4>
+                <p>{post.body}</p>
+                <div className="commentCount">댓글 수: {post.comment_cnt}</div>
+                <div className="postTime">{calcTime(post.created_at)}</div>
+              </div>
+            ))}
+          {isLoggedIn && (
+            <div className="writeButtonContainer">
+              <button onClick={goToPostingPage} className="writeButton">
+                글쓰기
+              </button>
             </div>
+          )}
         </div>
-    );
+        <div className="sidebar">
+          <h4>핫게</h4>
+          <ul>
+            {hotPosts.map((post, index) => (
+              <li
+                key={post.id}
+                onClick={() => goToHotPostDetail(post.id)}
+                style={{ cursor: "pointer" }}
+              >
+                {index + 1}등: {post.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
