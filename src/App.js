af4ebe8191dbac13431db10475ad70d982188227
jcy010905 <jcy010905@naver.com>
2024-01-11 10:20:02 +0900

    Signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SignUp from "./SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>찬용쓰</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  return( 
+    <SignUp />
+  )
 }
-
-export default App;
---
+++ src/SignUp.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./SignUp.css"
+
+export default function SignUp() {
+
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    function registerHandler() {
+
+        if(email == "" || nickname == "" || password == "" || passwordConfirm == "" ) {
+            alert("모든 정보를 입력해주세요!");
+            return;
+        }
+
+        if(password !== passwordConfirm) {
+            alert("비밀번호를 다르게 입력했습니다!");
+            return;
+        }
+
+        fetch(`http://back.mongjo.xyz/user/signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify({
+                email: email,
+                nickname: nickname,
+                password: password,
+                passwordConfirm: passwordConfirm
+            })
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res);
+            if(res["success"] == false) {
+                alert(res["data"]);
+            } else {
+                alert(res["data"]);
+            }
+        });
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="login-wrapper">
+                <h2>회원가입</h2>
+                    <form id="login-form" onSubmit={registerHandler}>
+                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="text" placeholder="Nickname" onChange={(e) => setNickname(e.target.value)} />
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                        <input type="password" placeholder="Password Confirm" onChange={(e) => setPasswordConfirm(e.target.value)} />
+                        <input
+                            type="submit"
+                            value="회원가입"
+                            />
+                        <p><a href="/">로그인 화면으로 돌아가기</a></p>
+                    </form>
+            </div>
+        </div>
+    );
+}
