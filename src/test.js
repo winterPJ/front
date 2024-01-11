53ac120022e123f29ab21dbdaa167bb8d8194b64
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-11 10:37:12 +0900

    merge chan

--- src/test.js
+++ src/test.js
@@ -1,11 +1,9 @@
-import React from "react"
+import SignUp from "./SignUp";
 
-export default function test(props) {
-
-    return (
-        <div>
-            <h3>Test!!!!</h3>
-            <h4>welcome, {props.name}, {props.age} </h4>
-        </div>
+export default function Test() {
+    return(
+        <>
+        <SignUp></SignUp>
+        </>
     )
 }
